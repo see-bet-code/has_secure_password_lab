d2e40e59493b2130e50ff87065cc3840f9597677
see-bet-code <brittani.elan.taylor@gmail.com>
2020-10-19 16:30:30 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to '/users/new' unless @user.save
+    session[:user_id] = @user.id
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :password,
+      :password_confirmation
+    )
+  end
+end
